dc0ab3d46fb40d8ce708e0a89e2f162d4bd64d00
Kamil Narygin <kamilsiberiya@gmail.com>
2019-05-20 02:22:49 +0700

    доделанное первое задание

---
+++ main.js
@@ -0,0 +1,6 @@
+function SummNumber() {
+var FirstNumber = + document.getElementById("first").value;
+var SecondNumber = + document.getElementById("second").value;
+var Summ = FirstNumber +SecondNumber;
+alert(Summ);
+}
